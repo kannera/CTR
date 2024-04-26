a373b950e66dcb66787f7e73d59d3165037a8f45
Antti Kanner <antti.kanner@helsinki.fi>
2024-04-26 14:48:41 +0300

    Update survey_bits.py

--- survey_bits.py
+++ survey_bits.py
@@ -4,4 +4,5 @@ import pandas
 def u_test(by_group):
   df = pandas.read_csv("https://raw.githubusercontent.com/kannera/CTR/main/kyselydata.tsv", sep="\t")
   values = list(df[by_group].drop_duplicates().values)
-  print(mannwhitneyu(df[df[by_group] == values[0]]['vastaus 2'].values, df[df[by_group] == values[1]]['vastaus 2'].values))
+  result = mannwhitneyu(df[df[by_group] == values[0]]['vastaus 2'].values, df[df[by_group] == values[1]]['vastaus 2'].values)
+  return result
