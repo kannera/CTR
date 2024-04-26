52cd83a8418d40062ba81299637967daef00f3d8
Antti Kanner <antti.kanner@helsinki.fi>
2024-04-26 14:32:36 +0300

    Create survey_bits.py

---
+++ survey_bits.py
@@ -0,0 +1,8 @@
+from scipy.stats import mannwhitneyu
+import pandas
+
+df = pandas.read_csv("https://raw.githubusercontent.com/kannera/CTR/main/kyselydata.tsv", sep="\t")
+
+by_group = "lukijaryhmä"
+values = list(df[by_group].drop_duplicates().values)
+print(mannwhitneyu(df[df[by_group] == values[0]]['vastaus 2'].values, df[df[by_group] == values[1]]['vastaus 2'].values))
